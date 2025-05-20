347ecee889c46e3e45f2e2dd6897732c578c142b
Максим Володько <maksimvolodko41@gmail.com>
2025-05-20 23:34:32 +0300

    Разработка

--- main-project/process.cs
+++ main-project/process.cs
@@ -6,34 +6,25 @@ using System.Threading.Tasks;
 
 namespace main_project
 {
-
     public class process
     {
-        public TimeSpan test(string[,] mas)
+        public DateTime fun(string[,] mass)
         {
-            int count = mas.GetLength(0);
+            int count = mass.getLength(0);
             TimeSpan max = new TimeSpan(00, 00, 00);
-            TimeSpan nol = new TimeSpan(00, 00, 00);
             for (int i = 0; i < count; i++)
             {
                 DateTime start1 = Convert.ToDateTime(mas[i, 0]);
                 DateTime finish1 = Convert.ToDateTime(mas[i, 1]);
                 TimeSpan raz1 = finish1.Subtract(start1);
+                
+                if (raz1 > max)
+                {
+                    max = raz1;
+                }
 
-                if (max == nol)
-                {
-                    max = raz1;
-                }
-                else if (raz1 < max)
-                {
-                    max = raz1;
-                }
-                
-                
             }
-
             return max;
-
         }
     }
 }
